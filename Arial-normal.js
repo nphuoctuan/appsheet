40598f670d5881209cddac42ccc22d3403a66da6
nphuoctuan <104477664+nphuoctuan@users.noreply.github.com>
2024-05-30 16:41:30 +0700

    Update Arial-normal.js

--- Arial-normal.js
+++ Arial-normal.js
@@ -4,4 +4,4 @@ var callAddFont = function () {
 this.addFileToVFS('Arial-normal.ttf', font);
 this.addFont('Arial-normal.ttf', 'Arial', 'normal');
 };
-jsPDF.API.events.push(['addFonts', callAddFont])
+jspdf.jsPDF.API.events.push(['addFonts', callAddFont])
